413752776880201a11a7233f57b80dafebdd4694
DESKTOP-C5V7M4P\young <youngjun0528>
2021-02-19 16:46:59 +0900

    python boot camp add

---
+++ PythonBootCamp/Sol_006.py
@@ -0,0 +1,11 @@
+# True & False
+
+print(True if None else False)
+
+print(True if 1 else False)
+
+print(True if "" else False)
+
+print(True if 0 else False)
+
+print(True if bool(0) else False)
---
+++ PythonBootCamp/Sol_008.py
@@ -0,0 +1,5 @@
+# Dictionaly 사용
+
+d = {'height': 100, 'weight': 70, 'temparture':36, 'eyesight': 1}
+
+print(d['weight'])
