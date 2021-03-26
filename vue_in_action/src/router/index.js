aa340e4d8614a88eb95272b7d59ebd443d8179bd
윤영준 <youngjun0528@gmail.com>
2021-03-26 00:05:26 +0000

    add

--- vue_in_action/src/router/index.js
+++ vue_in_action/src/router/index.js
@@ -1,27 +1,47 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import Form from "@/components/Form";
+import Main from "@/components/Main";
+import Product from "@/components/Product";
+import EditProduct from "@/components/EditProduct";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "iMain",
+    component: Main,
+    props: true
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/form",
+    name: "Form",
+    component: Form,
+    props: true
+  },
+  {
+    path: "/product/:id",
+    name: "Id",
+    component: Product,
+    props: true,
+    children: [
+      {
+        path: "edit",
+        name: "Edit",
+        component: EditProduct,
+        props: true
+      }
+    ]
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
 ];
 
-const router = new VueRouter({
+const router = new Router({
+  mode: "history",
   routes
 });
 
