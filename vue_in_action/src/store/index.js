1aa6186a23408c5a1557be0c809308450f522ead
윤영준 <youngjun0528@gmail.com>
2021-04-02 07:19:47 +0000

    vuex index add

--- vue_in_action/src/store/index.js
+++ vue_in_action/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import products from 'products';
+import products from "./products";
 
 Vue.use(Vuex);
 
@@ -8,5 +8,5 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: { products}
+  modules: { products },
 });
