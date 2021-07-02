8cab3592d6c85269f66a651596c5ed4c78157919
윤영준 <youngjun0528@gmail.com>
2021-07-02 09:43:44 +0000

    TaskAgile Add

---
+++ TaskAgile/app/front-end/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LoginPage from '../views/LoginPage.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  }]
+})
+
+export default router
