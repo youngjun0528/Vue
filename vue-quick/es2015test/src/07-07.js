92d23afbd49be75b9596c4368b585972106c4107
youngjun0528 <youngjun0528@gmail.com>
2023-02-06 16:04:11 +0900

    ex 07-07 : 템플릿 리터널

---
+++ vue-quick/es2015test/src/07-07.js
@@ -0,0 +1,9 @@
+var d1 = new Date();
+var name = "홍 길 동 ";
+var r1 = `${name} 님에게 ${d1.toDateString()} 에 연락했다`;
+console.log(r1);
+
+var product = "갤 럭 시 S7";
+var price = 199000;
+var str = `${product} 의 가격은 ${price}원입니다.`;
+console.log(str);
