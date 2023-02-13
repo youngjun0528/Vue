0c1884180b2d511c45ed8bdbb9a8cd96be5c5a1e
youngjun0528 <youngjun0528@gmail.com>
2023-02-13 14:29:49 +0900

    ex 09-04 : Dynamic Component

---
+++ vue-quick/dynamictest/src/components/TestAbout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <h3>{{ (new Date()).toTimeString() }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestAbout"
+}
+</script>
+
+<style scoped>
+
+</style>
