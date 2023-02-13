ed8bd447e5c30fe16c31b7708cf8d3c8d8ae5801
youngjun0528 <youngjun0528@gmail.com>
2023-02-13 15:01:10 +0900

    ex 09-05 : Recursive Component

--- vue-quick/dynamictest/src/App.vue
+++ vue-quick/dynamictest/src/App.vue
@@ -47,7 +47,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .header {
   background-color: aqua;
   padding: 10px 0px 0px Opx;
--- vue-quick/dynamictest/src/components/TestAbout.vue
+++ vue-quick/dynamictest/src/components/TestAbout.vue
@@ -2,15 +2,65 @@
   <div>
     <h1>About</h1>
     <h3>{{ (new Date()).toTimeString() }}</h3>
+    <h4>조직도</h4>
+    <TestTree :subs="orgcharts"></TestTree>
   </div>
 </template>
 
 <script>
+import TestTree from './TestTree.vue'
+
 export default {
-  name: "TestAbout"
+  name: "TestAbout",
+  components: {TestTree},
+  data: function () {
+    return {
+      orgcharts: [
+        {
+          name: "( 주 ) OpenSG",
+          type: "company",
+          subs: [
+            {
+              name: "SI 사업부", type: "division",
+              subs: [
+                {name: "SI 1 팀 ", type: "team"},
+                {name: "SI 2 팀 ", type: "team"}
+              ]
+            },
+            {
+              name: "BI 사업부", type: "division",
+              subs: [
+                {name: "BI 1 팀 ", type: "team"},
+                {name: "BI 2 팀 ", type: "team"},
+                {name: "BI 3 팀 ", type: "team"},
+              ]
+            },
+            {
+              name: "솔루션 사업부", type: "division",
+              subs: [
+                {name: "ESM 팀 ", type: "team"},
+                {name: "MTS 팀 ", type: "team"},
+                {name: "ASF 팀 ", type: "team"},
+              ]
+            },
+            {name: "총무팀", type: "division"},
+            {name: "임사팀", type: "division"}
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+li.company{
+  color: blue;
+}
+li.division {
+  color: steelblue;
+}
+li.team{
+  color: tomato;
+}
 </style>
