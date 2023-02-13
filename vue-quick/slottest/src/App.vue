f936a3e7a4f4ae3b9797a7772be9d77c436c88ba
youngjun0528 <youngjun0528@gmail.com>
2023-02-13 14:15:32 +0900

    ex 09-03 : Component Slot

---
+++ vue-quick/slottest/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <speech-box :headerText="A.header" :footerText="A.footer">
+      <div>
+        <p>
+          {{ A.message }}
+        </p>
+      </div>
+    </speech-box>
+    <speech-box class="sanders" :headerText="B.header" :footerText="B.footer ">
+      <div>
+        <p class="sanders-content">
+          {{ B.message }}
+        </p>
+      </div>
+    </speech-box>
+  </div>
+</template>
+
+<script>
+import SpeechBox from './components/SpeechBox.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SpeechBox
+  },
+  data: function () {
+    return {
+      A: {
+        header: "오 바 마 대통령 고별 연 설 문 ",
+        footer: "2017.01.10 - 시 카 고 ",
+        message: '저 의 동료 국민 여 러 분 ................... (지 면 관 계 상 생 략 )'
+      },
+      B: {
+        header: "버 니 샌 더 스 경선 패배 연 설 문 ",
+        footer: "2016.07.25- 필라델피아 웰 스 파 고 ",
+        message: ' 감사합니다. 여러분 정말 감사합니다.............(지면 관계상 생략)'
+      }
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.sanders {
+  background-color: antiquewhite;
+}
+
+.sanders-content {
+  text-decoration: underline;
+}
+</style>
---
+++ vue-quick/slottest/src/AppScoped.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="parent">
+    <child>
+      <template slot="type1" scope="p1">
+        <div>{{ p1.cx }} + {{ p1.cy }} =
+          {{ parseInt(p1.cx) + parseInt(p1.cy) }}
+        </div>
+      </template>
+      <template slot="type2" scope="p2">
+        <div>{{ p2.cx }} 더하기 {{ p2.cy }} 는
+          {{ parseInt(p2.cx) + parseInt(p2.cy) }}입 니 다 .
+        </div>
+      </template>
+    </child>
+  </div>
+</template>
+
+<script>
+import Child from './components/ScopedSlot.vue'
+
+export default {
+  name: "AppScoped",
+  components: {Child}
+}
+</script>
+
+<style scoped>
+.parent {
+  border: dashed 2px black;
+  padding: 5px;
+}
+</style>
