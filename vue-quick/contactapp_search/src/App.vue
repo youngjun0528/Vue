d4dfd2b9e9db0ea9e28ce779e383f1918286b581
youngjun0528 <youngjun0528@gmail.com>
2023-02-20 18:20:56 +0900

    ex 11-05 : Vuex Actions(비동기)

---
+++ vue-quick/contactapp_search/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <contact-search placeholder="두글자 이상 입력후 엔 터 !"></contact-search>
+    <contact-list></contact-list>
+  </div>
+</template>
+
+<script>
+import ContactList from './components/ContactList.vue'
+import ContactSearch from './components/ContactSearch.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactList, ContactSearch
+  }
+}
+</script>
+
+<style>
+</style>
