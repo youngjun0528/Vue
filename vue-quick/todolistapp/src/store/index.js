12bed9983b926b8dbc6fcf97ee6bfd827ef47ac5
youngjun0528 <youngjun0528@gmail.com>
2023-02-20 18:38:29 +0900

    ex 11-06 : Vuex divide

--- vue-quick/todolistapp/src/store/index.js
+++ vue-quick/todolistapp/src/store/index.js
@@ -1,45 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Constant from '../Constant';
+import state from "@/store/state";
+import mutations from "@/store/mutations";
+import actions from "@/store/actions";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        todolist: [
-            {todo: "영 화 보 기 ", done: false},
-            {todo: "주 말 산 책 ", done: true},
-            {todo: "ES6 학 습 ", done: false},
-            {todo: "잠 실 야 구 장 ", done: false}
-        ]
-    },
-    mutations: {
-        [Constant.ADD_TODO]: (state, payload) => {
-            if (payload.todo !== "") {
-                state.todolist.push({todo: payload.todo, done: false});
-            }
-        },
-        [Constant.DONE_TOGGLE]: (state, payload) => {
-            state.todolist[payload.index].done = !state.todolist[payload.index].done;
-        },
-        [Constant.DELETE_TODO]: (state, payload) => {
-            state.todolist.splice(payload.index, 1);
-        }
-    },
-    actions: {
-        [Constant.ADD_TODO]: (state, payload) => {
-            console.log("### addTodo", payload);
-            store.commit(Constant.ADD_TODO, payload);
-        },
-        [Constant.DONE_TOGGLE]: (state, payload) => {
-            console.log("### doneToggle", payload);
-            store.commit(Constant.DONE_TOGGLE, payload);
-        },
-        [Constant.DELETE_TODO]: (state, payload) => {
-            console.log("### deleteTodo", payload);
-            store.commit(Constant.DELETE_TODO, payload);
-        }
-    }
+    state,
+    mutations,
+    actions
 })
 
 export default store
