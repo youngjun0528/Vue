a91a2e17b481219c6ae8a4128e4c41afa1fa70c5
youngjun0528 <youngjun0528@gmail.com>
2023-02-27 16:09:08 +0900

    ex 13-08 : CSS Transition ContactApp

--- vue-quick/contactapp/src/App.vue
+++ vue-quick/contactapp/src/App.vue
@@ -9,7 +9,9 @@
         <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
       </div>
     </div>
-    <router-view></router-view>
+    <transition name="flip" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <loading v-show="isloading"></loading>
   </div>
 </template>
@@ -38,4 +40,19 @@ export default {
 .menu {
   width: 100px;
 }
+
+.flip-enter-active {
+  transition: all .4s ease-in-out;
+}
+
+.flip-leave-active {
+  transition: all .4s ease-in-out;
+}
+
+.flip-enter, .flip-leave-to {
+  transform: scaleY(0) translateZ(0);
+  opacity: 0;
+}
+
+
 </style>
--- vue-quick/contactapp/src/components/ContactList.vue
+++ vue-quick/contactapp/src/components/ContactList.vue
@@ -45,7 +45,12 @@
               :container-class="'pagination'"
               :page-class="'page-item'">
     </paginate>
-    <router-view></router-view>
+    <transition
+        v-on:before-enter="beforeEnter"
+        v-on:enter="enter"
+        v-on:leave="leave">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -53,6 +58,7 @@
 import Constant from '../Constant';
 import {mapState} from 'vuex';
 import Paginate from 'vuejs-paginate';
+import Velocity from 'velocity-animate'
 
 export default {
   name: 'contactList',
@@ -95,6 +101,20 @@ export default {
     },
     editPhoto: function (no) {
       this.$router.push({name: 'updatephoto', params: {no: no}})
+    },
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+    },
+    enter: function (el, done) {
+      Velocity(el, {opacity: 0, scale: 0.2}, {duration: 200})
+      Velocity(el, {opacity: 0.7, scale: 1.2}, {duration: 200})
+      Velocity(el, {opacity: 1, scale: 1}, {complete: done})
+    },
+    leave: function (el, done) {
+      Velocity(el, {translateX: '0px', opacity: 1}, {duration: 100})
+      Velocity(el, {translateX: '20px', opacity: 1}, {duration: 100, loop: 2})
+      Velocity(el, {translateX: '0px', opacity: 1}, {duration: 200})
+      Velocity(el, {translateX: '100px', opacity: 0}, {complete: done})
     }
   }
 }
